929ad64420b04fa730dcb67f37e8ed8f8dfadf36
ToshifumiSasaki <sasaki@tlu.ee>
2018-11-20 22:07:54 +0200

    first commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // press <- key
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0); //move left 3
+
+        }
+
+        // if press -> key
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0); // move right 3
+        }
+
+	}
+}
